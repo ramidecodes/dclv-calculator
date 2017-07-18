3a83b79f5ae1ccf3527f05d3a30ec20161469376
Ecovirtual <ramiro.ecovirtual@gmail.com>
2017-07-18 15:57:26 -0300

    Add tools folder to repo

--- src/components/AboutPage.spec.js
+++ src/components/AboutPage.spec.js
@@ -3,14 +3,6 @@ import {shallow} from 'enzyme';
 import AboutPage from './AboutPage';
 
 describe('<AboutPage />', () => {
-  // it('should have a header called \'Descripción del Proyecto\'', () => {
-  //   const wrapper = shallow(<AboutPage />);
-  //   const actual = wrapper.find('h1').text();
-  //   const expected = 'Descripción del Proyecto';
-  //
-  //   expect(actual).toEqual(expected);
-  // });
-
   it('should have a header with \'alt-header\' class', () => {
     const wrapper = shallow(<AboutPage />);
     const actual = wrapper.find('h1').prop('className');
