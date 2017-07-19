5ce9391e9083748edeac6c5273559e62f6ea0e2e
Ecovirtual <ramiro.ecovirtual@gmail.com>
2017-07-19 18:41:58 -0300

    update placeholders for Common Components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link, IndexLink } from 'react-router';
+// import Header from './Header';
+// import Footer from './Footer';
 
 // This is a class-based component because the current
 // version of hot reloading won't hot reload a stateless
@@ -8,12 +10,16 @@ import { Link, IndexLink } from 'react-router';
 class App extends React.Component {
   render() {
     return (
+      // Add main component class to this div
       <div>
+        // <Header/>
         <IndexLink to="/">Inicio</IndexLink>
         {' | '}
         <Link to="/about">Descripción General</Link>
         <br/>
         {this.props.children}
+
+        // <Footer/>
       </div>
     );
   }
