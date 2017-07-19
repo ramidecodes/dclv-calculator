ec0e4f2c571dc4be4def57acec2629ccd8129026
Ecovirtual <ramiro.ecovirtual@gmail.com>
2017-07-19 13:21:39 -0300

    Add Production Server and dist folder to repo

--- tools/prodServer.js
+++ tools/prodServer.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var open = require('open');
 var compression = require('compression');
-var bodyParser = require('body-parser');
 /* eslint-disable no-console, no-unused-vars */
 
 process.env.NODE_ENV = 'production'
@@ -10,13 +9,10 @@ process.env.NODE_ENV = 'production'
 var port = 3000;
 var app = express();
 
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
 app.use(compression());
 app.use(express.static('dist'));
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 app.listen(port, function(err){
