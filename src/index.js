4265390b4b4f0a90c1804c17d08ad1520cb13fc7
Ecovirtual <ramiro.ecovirtual@gmail.com>
2017-07-20 12:18:49 -0300

    Add Semantic UI as styles framework

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/default */
-
+// Entry Point for the App
 import React from 'react';
 import { render } from 'react-dom';
 import { browserHistory } from 'react-router';
@@ -9,6 +9,7 @@ import Root from './components/Root';
 import configureStore from './store/configureStore';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
 import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
+import 'semantic-ui-css/semantic.min.css'; // Include minified css from Semantic UI
 import { syncHistoryWithStore } from 'react-router-redux';
 
 const store = configureStore();
